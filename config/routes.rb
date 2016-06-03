d568489abb204995cd1501b373d3f0b34933f522
bradracz <bradley.racz@gmail.com>
2016-06-03 11:50:05 +0000

    adding items with nested attributes

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,10 @@ Rails.application.routes.draw do
 
   get 'welcome/features'
 
-  resources :invoices
-    root to: 'welcome#index'
+  resources :invoices do
+    resources :purchases, except: [:index], controller: 'invoices/purchases'
   end
+  
+  root to: 'welcome#index'
+  
+end
